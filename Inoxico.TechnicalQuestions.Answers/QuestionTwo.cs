1de58e9458a0cf247e9fbd8246a76c51d4fb1ab2
johnojo97 <jojo@retrorabbit.co.za>
2023-02-01 21:34:09 +0200

    Added some of decline logic

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,7 +5,40 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            throw new NotImplementedException();
+            if (points == null)
+                throw new Exception("points is null");
+
+            var deepestPit = -1;
+
+            var pointsLength = points.Length;
+            for (int i = 0; i < pointsLength-1; i++)
+            {
+                if (points[i] < points[i + 1])
+                {
+
+                }
+                else if (points[i] > points[i + 1])
+                {
+                    var declineInitial = points[i] - points[i + 1];
+                    var hitIncline = false;
+                    for (i = i + 1; i < pointsLength - 1; i++)
+                    {
+                        if (points[i] < points[i + 1])
+                        {
+                            deepestPit = Math.Min(declineInitial, points[i + 1] - points[i]);
+                            hitIncline = true;
+                        }
+
+                        if (hitIncline && points[i] >= points[i + 1])
+                        {
+                            i--;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return deepestPit;
         }
     }
 }
