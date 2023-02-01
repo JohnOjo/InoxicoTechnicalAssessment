7995150946d8f5a14e990ecec31786fc0a912dd7
johnojo97 <jojo@retrorabbit.co.za>
2023-02-01 21:00:55 +0200

    Implemented GetLongestSentance

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,20 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            string [] splitByPunctuation = s.Split(new char[] { '.', '!', '?' });
+
+            var maxWords = 0;
+            foreach (string word in splitByPunctuation)
+            {
+                var words = word.Split(' ');
+                var wordLength = word.Length;
+                if (maxWords < wordLength)
+                {
+                    maxWords = wordLength;
+                }
+            }
+
+            return maxWords;
         }
     }
 }
